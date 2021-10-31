4a11d592ec026502743587d77b43e1bdb706cfef
Marvin Munoz Baron <mmunozba@outlook.com>
2021-10-31 19:50:35 +0100

    Return at / JSON in backend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return {
+        "lastmessage": "Goodbye",
+    }
 
 
 if __name__ == '__main__':
