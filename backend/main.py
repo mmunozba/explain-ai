7969a180587fed9657d76f5040758f74f4c2c3e7
Marvin Munoz Baron <mmunozba@outlook.com>
2021-10-31 21:51:22 +0100

    Implement a minimal prototype (#3)
    * Return at / JSON in backend
    
    * Fix communication in the frontend
    
    * Implement working minimal prototype

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, send_file
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import pandas as pd
+import shap
+import json
 
 app = Flask(__name__)
+model = keras.models.load_model('model/trained-regressor')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return {
+        "lastmessage": "Goodbye",
+    }
+
+
+@app.route('/explain')
+def explain():
+    X, y = shap.datasets.adult()
+    X_display, y_display = shap.datasets.adult(display=True)
+    explainer = shap.KernelExplainer(f, X.iloc[:50, :])
+    shap_values = explainer.shap_values(X.iloc[299, :], nsamples=500)
+    shap.force_plot(explainer.expected_value, shap_values, X_display.iloc[299, :], show=False, matplotlib=True)\
+        .savefig('scratch.png')
+    return send_file('scratch.png', mimetype='image/jpeg')
+
+@app.route('/predict')
+def predict():
+    X, y = shap.datasets.adult()
+    X_display, y_display = shap.datasets.adult(display=True)
+    explainer = shap.KernelExplainer(f, X.iloc[:50, :])
+    shap_values = explainer.shap_values(X.iloc[299, :], nsamples=500)
+    return { # from https://stackoverflow.com/a/44752209
+        "expectedvalue": pd.Series(explainer.expected_value).to_json(orient='values'),
+        "shapvalues": pd.Series(shap_values).to_json(orient='values'),
+    }
+
+
+def f(X):
+    return model.predict([X[:, i] for i in range(X.shape[1])]).flatten()
 
 
 if __name__ == '__main__':
---
+++ backend/train.py
@@ -0,0 +1,59 @@
+# Code from https://shap.readthedocs.io/en/latest/example_notebooks/tabular_examples/neural_networks/Census%20income%20classification%20with%20Keras.html
+# --------------------------------------
+# Initialization
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Input, Dense, Flatten, Concatenate, concatenate, Dropout, Lambda, Embedding
+from tensorflow.keras.models import Model
+from tqdm import tqdm
+import shap
+
+# print the JS visualization code to the notebook
+shap.initjs()
+
+
+# --------------------------------------
+# Load dataset
+
+X,y = shap.datasets.adult()
+X_display,y_display = shap.datasets.adult(display=True)
+
+# normalize data (this is important for model convergence)
+dtypes = list(zip(X.dtypes.index, map(str, X.dtypes)))
+for k,dtype in dtypes:
+    if dtype == 'float32':
+        X[k] -= X[k].mean()
+        X[k] /= X[k].std()
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=7)
+
+
+# --------------------------------------
+# Train Keras model
+
+# build model
+input_els = []
+encoded_els = []
+for k,dtype in dtypes:
+    input_els.append(Input(shape=(1,)))
+    if dtype == 'int8':
+        e = Flatten()(Embedding(X_train[k].max()+1, 1)(input_els[-1]))
+    else:
+        e = input_els[-1]
+    encoded_els.append(e)
+encoded_els = concatenate(encoded_els)
+layer1 = Dropout(0.5)(Dense(100, activation='relu')(encoded_els))
+out = Dense(1)(layer1)
+
+# train model
+regression = Model(inputs=input_els, outputs=[out])
+regression.compile(optimizer='adam', loss='binary_crossentropy')
+regression.fit(
+    [X_train[k].values for k,t in dtypes],
+    y_train,
+    epochs=50,
+    batch_size=512,
+    shuffle=True,
+    validation_data=([X_valid[k].values for k,t in dtypes], y_valid)
+)
+
+regression.save('model/trained-regressor')
